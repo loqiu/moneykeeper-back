4ce86459fae7bed1bc3fb9fdc8c6240f8f8e1ae9
Rochelle <loqiu.software@gmail.com>
2026-03-13 14:38:00 +0000

    fix: align legacy type migration collation

--- src/main/resources/db/migration/V6__normalize_legacy_type_values.sql
+++ src/main/resources/db/migration/V6__normalize_legacy_type_values.sql
@@ -6,18 +6,26 @@ ALTER TABLE `moneykeeper`
 
 UPDATE `categories`
 SET `type` = CASE
-  WHEN `type` = CONVERT(0xE694B6E585A5 USING utf8mb4) THEN 'income'
-  WHEN `type` = CONVERT(0xE694AFE587BA USING utf8mb4) THEN 'expense'
-  WHEN LOWER(`type`) = 'income' THEN 'income'
-  WHEN LOWER(`type`) = 'expense' THEN 'expense'
+  WHEN `type` = (CONVERT(0xE694B6E585A5 USING utf8mb4) COLLATE utf8mb4_unicode_ci)
+    THEN _utf8mb4'income' COLLATE utf8mb4_unicode_ci
+  WHEN `type` = (CONVERT(0xE694AFE587BA USING utf8mb4) COLLATE utf8mb4_unicode_ci)
+    THEN _utf8mb4'expense' COLLATE utf8mb4_unicode_ci
+  WHEN LOWER(`type`) = (_utf8mb4'income' COLLATE utf8mb4_unicode_ci)
+    THEN _utf8mb4'income' COLLATE utf8mb4_unicode_ci
+  WHEN LOWER(`type`) = (_utf8mb4'expense' COLLATE utf8mb4_unicode_ci)
+    THEN _utf8mb4'expense' COLLATE utf8mb4_unicode_ci
   ELSE `type`
 END;
 
 UPDATE `moneykeeper`
 SET `type` = CASE
-  WHEN `type` = CONVERT(0xE694B6E585A5 USING utf8mb4) THEN 'income'
-  WHEN `type` = CONVERT(0xE694AFE587BA USING utf8mb4) THEN 'expense'
-  WHEN LOWER(`type`) = 'income' THEN 'income'
-  WHEN LOWER(`type`) = 'expense' THEN 'expense'
+  WHEN `type` = (CONVERT(0xE694B6E585A5 USING utf8mb4) COLLATE utf8mb4_unicode_ci)
+    THEN _utf8mb4'income' COLLATE utf8mb4_unicode_ci
+  WHEN `type` = (CONVERT(0xE694AFE587BA USING utf8mb4) COLLATE utf8mb4_unicode_ci)
+    THEN _utf8mb4'expense' COLLATE utf8mb4_unicode_ci
+  WHEN LOWER(`type`) = (_utf8mb4'income' COLLATE utf8mb4_unicode_ci)
+    THEN _utf8mb4'income' COLLATE utf8mb4_unicode_ci
+  WHEN LOWER(`type`) = (_utf8mb4'expense' COLLATE utf8mb4_unicode_ci)
+    THEN _utf8mb4'expense' COLLATE utf8mb4_unicode_ci
   ELSE `type`
 END;
